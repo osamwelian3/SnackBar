0702acce1b4f0235adf7bbf8ac1f9cc2c5c767d0
Samuel Ian <osamwelian3@gmail.com>
2022-08-13 12:39:10 +0300

    Add database setting for connecting to mysql

--- SnackBar/settings.py
+++ SnackBar/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'SnackBar.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': Path.join(BASE_DIR, 'db.sqlite3'),
+    'default': { # Add database settings for mysql
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'SNACKBAR',
+        'USER': 'snackbar',
+        'PASSWORD': 'mariam',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
