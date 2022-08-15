6dc09cf77dc53a3527f5ae9e4e5962046d04128f
Samuel Ian <osamwelian3@gmail.com>
2022-08-15 10:11:25 +0300

    It will create user profile model

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=32, default='')
+    last_name = models.CharField(max_length=32, default='')
+    other_name = models.CharField(max_length=32, default='')
+    email = models.CharField(max_length=255, default='')
+    phone = models.CharField(max_length=12, default='')
+    city = models.CharField(max_length=255, default='')
+
+    class Meta:
+        db_table = 'UserProfile'
+
+    def __str__(self) -> str:
+        return self.first_name + ' ' + self.last_name
