ce82c3f63517513d770ae18e4b4b4ff258b85b74
Samuel Ian <osamwelian3@gmail.com>
2022-08-15 14:39:35 +0300

    It will add url pattern for email verification

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('get_csrf/', views.setCsrfCookie),
     path('register/', views.signup),
+    path('activate/<str:uidb64>/<str:token>/', views.activate, name='activate'),
 ]
