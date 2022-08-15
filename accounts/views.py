7c4495c3946b574e156b1312933659d3ad798fb2
Samuel Ian <osamwelian3@gmail.com>
2022-08-15 14:39:10 +0300

    ft: It will add verification email feature

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
+import imp
+from django.shortcuts import HttpResponse
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
 from django.http import JsonResponse
 from django.core.mail import send_mail
 from rest_framework.views import APIView
@@ -6,7 +11,9 @@ from rest_framework import permissions
 from rest_framework.response import Response
 from django.views.decorators.csrf import csrf_protect, ensure_csrf_cookie
 from SnackBar.serializer import UserSerializer
+from SnackBar.tokens import account_activation_token
 from django.contrib.auth.models import User
+from django.contrib.sites.shortcuts import get_current_site
 
 
 
@@ -42,7 +49,21 @@ def signup(request):
                         user.is_active = False
                         user.save()
 
-                        send_mail("Verification Code", "This is your test verification code: 1234", 'no-reply@snackbar.com', [email])
+                        current_site = get_current_site(request)
+                        mail_subject = 'Activate your account.'
+                        message = render_to_string('email_template.html', {
+                                    'user': user,
+                                    'domain': current_site.domain,
+                                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                                    'token': account_activation_token.make_token(user),
+                                })
+                        try:
+                            send_mail(mail_subject, message, 'no-reply@snackbar.com', [email])
+                        except Exception as e:
+                            print('Send_Mail_Exception: ' + str(e))
+                            return Response({'Detail': 'Account created but couldn\'t send activation email. Please contact admin at osamwelian3@gmail.com'})
+
+                        # send_mail("Verification Code", "This is your test verification code: 1234", 'no-reply@snackbar.com', [email])
 
                         user = User.objects.get(username=username)
                         user = UserSerializer(user)
@@ -50,3 +71,18 @@ def signup(request):
     except Exception as e:
         print('Exception: '+ str(e))
         return Response({"Error": "Something went wrong trying to sign you up. Contact osamwelian3@gmail.com"})
+
+@api_view(['GET'])
+def activate(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        response = render_to_string('redirect.html', { 'success': 'Thank you for your email confirmation. Now you can login your account.', 'site_url': str(request.scheme)+ '://' +str(get_current_site(request))})
+        return HttpResponse(response)
+    else:
+        return Response({'Failed': 'Activation link is invalid!'})
