b1ad3cf104c9198a816b6cda47a96130c85ca39f
Samuel Ian <osamwelian3@gmail.com>
2022-08-16 07:34:49 +0300

    It will add path for the three endpoints, sign in, logout and isAthenticated

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('get_csrf/', views.setCsrfCookie),
+    path('get_auth_status/', views.isAuthenticated),
     path('register/', views.signup),
     path('activate/<str:uidb64>/<str:token>/', views.activate, name='activate'),
+    path('login/', views.signin),
+    path('logout/', views.log_out),
 ]
