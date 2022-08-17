d04355da4ad82e503772699e0f637a199ad4852c
Samuel Ian <osamwelian3@gmail.com>
2022-08-17 17:00:40 +0300

    It will add user profile serializer

--- SnackBar/serializer.py
+++ SnackBar/serializer.py
@@ -1,8 +1,15 @@
+from dataclasses import fields
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from user_profile.models import UserProfile
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'date_joined']
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = '__all__'
