faa8470c115fabbe6d1376963e095080cff2b1f3
Samuel Ian <osamwelian3@gmail.com>
2022-11-03 13:45:14 +0300

    trusted origins

--- SnackBar/settings.py
+++ SnackBar/settings.py
@@ -215,4 +215,4 @@ SESSION_COOKIE_NAME = "sessionkey"
 CSRF_COOKIE_HTTPONLY = False
 CSRF_COOKIE_SECURE = False
 
-CSRF_TRUSTED_ORIGINS =  ['http://*.localhost:8000', 'http://*.127.0.0.1:8000', 'http://localhost:8000', 'http://127.0.0.1:8000', 'http://localhost:3000', 'https://2769-154-159-237-88.in.ngrok.io'] # ['https://*.mydomain.com','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS =  ['http://*.localhost:8000', 'http://*.127.0.0.1:8000', 'http://localhost:8000', 'http://127.0.0.1:8000', 'http://localhost:3000', 'http://shop-mall.xyz', 'http://*.shop-mall.xyz', 'http://api.shop-mall.xyz'] # ['https://*.mydomain.com','https://*.127.0.0.1']
