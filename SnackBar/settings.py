a24970d0bbfccb4a385f0f6da9ff378347569356
Samuel Ian <osamwelian3@gmail.com>
2022-11-04 16:20:58 +0300

    enable debug

--- SnackBar/settings.py
+++ SnackBar/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--9no341%7@qo)n(gfoz0$&@s1!xmhu6%4*3dsf8qtln*@q242@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
